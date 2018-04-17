84f3b8e8f692160ed2715c6f74e226ae9fe4e417
XQ <xq@XQ.local>
2018-04-17 21:50:20 +0800

    添加RNpackage

---
+++ RNApp/android/app/src/main/java/com/rnapp/RNPackage.java
@@ -0,0 +1,30 @@
+package com.rnapp;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import com.rnapp.rnmodules.NativeUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xq on 18/4/17.
+ */
+
+public class RNPackage implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList();
+        modules.add(new NativeUtil(reactContext));
+        return modules;
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        List<ViewManager> viewManagers = new ArrayList();
+        return viewManagers;
+    }
+}
